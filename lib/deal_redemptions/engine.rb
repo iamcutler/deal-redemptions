8fd38a67e464481f14c3b834d5df042133376401
Allan Cutler <musicfan03@gmail.com>
2014-07-10 14:15:59 -0700

    New engine configuration actions to allow overriding of default vars in the hosted application

--- lib/deal_redemptions/engine.rb
+++ lib/deal_redemptions/engine.rb
@@ -3,3 +3,22 @@ module DealRedemptions
     isolate_namespace DealRedemptions
   end
 end
+
+module DealRedemptions
+  # Maps the vars from hosted app into the engine
+  def self.configure(&block)
+    @@config ||= DealRedemptions::Engine::Configuration.new
+
+    # Default vars if not overwritten
+    @@config.site_name = 'Deal Site Redemptions'
+    @@config.logo = '/assets/logo.png'
+
+    yield @@config if block
+
+    return @@config
+  end
+
+  def self.config
+    Rails.application.config
+  end
+end
