299a18411a3dec200f3c3ed2075cea6accb2a07b
Allan Cutler <musicfan03@gmail.com>
2014-07-11 15:23:07 -0700

    Added default scope to only fetch active companies.

--- app/models/deal_redemptions/company.rb
+++ app/models/deal_redemptions/company.rb
@@ -2,6 +2,8 @@ module DealRedemptions
   class Company < ActiveRecord::Base
     validates :name, :slug, presence: true, uniqueness: true
 
+    default_scope { where(active: 1) }
+
     private
 
       def company_params
