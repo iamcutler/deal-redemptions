339bd7bc62b9dd6b33964ad46c50ed523a69e141
Allan Cutler <musicfan03@gmail.com>
2014-07-11 15:54:28 -0700

    Refactored company factories to isolate the namespace

--- spec/factories/company.rb
+++ spec/factories/company.rb
@@ -1,17 +1,19 @@
-FactoryGirl.define do
-  factory :company, class: DealRedemptions::Company do
-    name 'Groupon'
-    description 'Please use the 2nd code, not internal code.'
-    url 'http://www.groupon.com'
-    slug 'groupon'
-    active 1
-  end
+module DealRedemptions
+  FactoryGirl.define do
+    factory :company, class: Company do
+      name 'Groupon'
+      description 'Please use the 2nd code, not internal code.'
+      url 'http://www.groupon.com'
+      slug 'groupon'
+      active 1
+    end
 
-  factory :company2, class: DealRedemptions::Company do
-    name 'Living Social'
-    description 'Please use the 2nd code, not internal code.'
-    url 'http://www.livingsocial.com'
-    slug 'livingsocial'
-    active 1
+    factory :company2, class: Company do
+      name 'Living Social'
+      description 'Please use the 2nd code, not internal code.'
+      url 'http://www.livingsocial.com'
+      slug 'livingsocial'
+      active 1
+    end
   end
 end
