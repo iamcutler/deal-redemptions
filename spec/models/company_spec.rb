1dd7d113a35f53dccfad2a5bebeb0214c0ecca68
Allan Cutler <musicfan03@gmail.com>
2014-07-11 15:09:18 -0700

    Refactored company model validation spec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,30 +1,16 @@
 require 'rails_helper'
 
 describe DealRedemptions::Company, :type => :model do
-  describe 'validations' do
-    before(:each) do
-      @company = FactoryGirl.build(:company)
-    end
+  before(:each) do
+    @company = FactoryGirl.build(:company)
+  end
 
-    it 'fails if no name is present' do
-      @company.name = ''
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
 
-      expect(@company).to_not be_valid
-    end
-
-    it 'fails if no slug is present' do
-      @company.slug = ''
-
-      expect(@company).to_not be_valid
-    end
-
-    it 'fails if name is not unique' do
-      should validate_uniqueness_of(:name)
-    end
-
-    it 'fails if name is not unique' do
-      should validate_uniqueness_of(:slug)
-    end
+    it { should validate_presence_of(:slug) }
+    it { should validate_uniqueness_of(:slug) }
 
     it 'to pass with required attributes' do
       expect(@company).to be_valid
