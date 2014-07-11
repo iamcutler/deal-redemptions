c02bcdaffb2e9c5ee779d2fdee41a687e01d8126
Allan Cutler <musicfan03@gmail.com>
2014-07-11 12:02:04 -0700

    Added company validates and strong params

--- app/models/deal_redemptions/company.rb
+++ app/models/deal_redemptions/company.rb
@@ -1,4 +1,12 @@
 module DealRedemptions
   class Company < ActiveRecord::Base
+    validates :name, :slug, presence: true, uniqueness: true
+
+    private
+
+      def company_params
+        # Whitelist permitted parameters
+        params.require(:company).permit(:name, :description, :url, :slug, :active)
+      end
   end
 end
