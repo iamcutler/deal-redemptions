f39122ab1c897904fffbf865904fcd6d979baa79
Allan Cutler <musicfan03@gmail.com>
2014-07-14 09:43:11 -0700

    Removed namespacing on query call. No need since it's wrapped in module.

--- app/controllers/deal_redemptions/welcome_controller.rb
+++ app/controllers/deal_redemptions/welcome_controller.rb
@@ -3,7 +3,7 @@ require_dependency "deal_redemptions/application_controller"
 module DealRedemptions
   class WelcomeController < ApplicationController
     def index
-      @companies = DealRedemptions::Company.all.order(:name)
+      @companies = Company.all.order(:name)
     end
   end
 end
