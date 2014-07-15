4ac6347aff8166808b6f8acdb98f9b0c86760016
Allan Cutler <musicfan03@gmail.com>
2014-07-15 16:17:07 -0700

    New sessions controller and basic login view.

---
+++ app/controllers/deal_redemptions/admin/sessions_controller.rb
@@ -0,0 +1,16 @@
+require_dependency "deal_redemptions/application_controller"
+
+module DealRedemptions
+  class Admin::SessionsController < ApplicationController
+    layout '/deal_redemptions/admin/default'
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+  end
+end
---
+++ spec/controllers/deal_redemptions/admin/sessions_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+module DealRedemptions
+  RSpec.describe Admin::SessionsController, :type => :controller do
+    routes { DealRedemptions::Engine.routes }
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        expect(response).to be_success
+      end
+    end
+
+  end
+end
