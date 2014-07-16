298ba4c9d33bc44605cecbe11c41a430d9b2c090
Allan Cutler <musicfan03@gmail.com>
2014-07-16 16:02:23 -0700

    New auth layout introduced and used in sessions controller

--- app/controllers/deal_redemptions/admin/sessions_controller.rb
+++ app/controllers/deal_redemptions/admin/sessions_controller.rb
@@ -2,7 +2,7 @@ require_dependency "deal_redemptions/application_controller"
 
 module DealRedemptions
   class Admin::SessionsController < ApplicationController
-    layout '/deal_redemptions/admin/default'
+    layout '/deal_redemptions/admin/auth'
 
     def index
     end
