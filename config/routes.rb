93f496ca3d02dcd7a54adbae9e35457a62cd7955
Allan Cutler <musicfan03@gmail.com>
2014-07-16 16:03:36 -0700

    New admin routing namespace with dashboard and sessions login routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ DealRedemptions::Engine.routes.draw do
     get ':company', to: 'redeem#new', as: :redemption
     post ':company', to: 'redeem#create'
   end
+
+  # Admin
+  namespace :admin do
+    get '/', to: 'dashboard#index', as: 'dashboard'
+    get 'login', to: 'sessions#index', as: 'login'
+  end
 end
