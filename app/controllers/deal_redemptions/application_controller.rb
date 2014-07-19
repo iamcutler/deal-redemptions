dc416957dba5da6e974917278d45c965e7a2574d
Allan Cutler <musicfan03@gmail.com>
2014-07-18 23:28:18 -0700

    Modified current user helper method to be current admin user for better clarification.

--- app/controllers/deal_redemptions/application_controller.rb
+++ app/controllers/deal_redemptions/application_controller.rb
@@ -1,11 +1,11 @@
 module DealRedemptions
   class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :current_admin_user
 
     private
 
-    def current_user
-      @current_user ||= DealRedemptions::User.find(session[:admin_user_id]) if session[:admin_user_id]
+    def current_admin_user
+      @current_admin_user ||= DealRedemptions::User.find(session[:admin_user_id]) if session[:admin_user_id]
     end
   end
 end
