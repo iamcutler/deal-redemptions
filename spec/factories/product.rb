c9ed24873f3515be40a04d2d85dc55e9344540c2
Allan Cutler <musicfan03@gmail.com>
2014-07-22 12:18:45 -0700

    Added id to product factory

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -1,6 +1,7 @@
 module DealRedemptions
   FactoryGirl.define do
     factory :product, class: Product do
+      id 1
       name 'Smile Sciences Teeth Whitening Pen'
       url 'http://smilesciences.com/collections/products/products/smile-sciences-teeth-whitening-pen'
     end
