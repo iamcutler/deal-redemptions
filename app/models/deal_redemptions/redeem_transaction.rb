2109479e2e8b237ea3d209620b2eabf80d91c980
Allan Cutler <musicfan03@gmail.com>
2014-07-28 11:47:59 -0700

    Added foreign key to redemption relationship and access to redeem codes through redemption model for transactions.

--- app/models/deal_redemptions/redeem_transaction.rb
+++ app/models/deal_redemptions/redeem_transaction.rb
@@ -1,6 +1,7 @@
 module DealRedemptions
   class RedeemTransaction < ActiveRecord::Base
     belongs_to :company
-    has_many :redemption
+    has_many :redemption, foreign_key: 'transaction_id'
+    has_many :redeem_code, through: :redemption
   end
 end
