48d9455a04547215582f2d382f18ed67275cfcae
Allan Cutler <musicfan03@gmail.com>
2014-08-04 15:22:31 -0700

    Added accessor for mailing list to redemption model

--- app/models/deal_redemptions/redemption.rb
+++ app/models/deal_redemptions/redemption.rb
@@ -2,6 +2,8 @@ module DealRedemptions
   class Redemption < ActiveRecord::Base
     has_many :redeem_code
 
+    attr_accessor :mailing_list
+
     accepts_nested_attributes_for :redeem_code, update_only: true
 
     validates :first_name, :last_name, :email_address, :address1, :city, :state, :zip_code, :country, presence: true
