012badb6e26e0ab9218cef439db308fd7fdd3f14
Allan Cutler <musicfan03@gmail.com>
2014-08-07 22:23:29 -0700

    Changed logo name in engine rake file config template

--- config/config_engine_rake_file.rb
+++ config/config_engine_rake_file.rb
@@ -3,7 +3,7 @@ DealRedemptions.configure do |config|
   # config.site_name = "Smile Sciences Redemptions"
 
   # Set Smile Sciences logo
-  # config.logo = '/assets/logo.png'
+  # config.logo = '/assets/deal-redemptions-logo.png'
 
   # Disable powered by credit
   # config.disable_powered = true
